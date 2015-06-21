d441c4869d473a1904cf6c7a97a15155169fc92e
Devin Clark <notdevinclark@gmail.com>
2015-06-20 21:22:03 -0500

    updates the nest_controller to use the new echo_request class

--- app/controllers/nest_controller.rb
+++ app/controllers/nest_controller.rb
@@ -1,25 +1,6 @@
 class NestController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
   def create
-    render json: { version: '1.0',
-                   response: {
-                     outputSpeech: {
-                       type: 'PlainText',
-                       text: 'Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?'
-                     },
-                     card: {
-                       type: 'Simple',
-                       title: 'Horoscope',
-                       content: 'Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless.'
-                     },
-                     reprompt: {
-                       outputSpeech: {
-                         type: 'PlainText',
-                         text: 'Can I help you with anything else?'
-                       }
-                     },
-                     shouldEndSession: true
-                   }
-                 }.to_json
+    render json: EchoRequest.new(params['nest']).response
   end
 end
